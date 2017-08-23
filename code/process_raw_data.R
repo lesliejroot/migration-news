2def3a64ed14d1964ecdc39cd9f0aa07321c08d8
mjalexander <monicaalexander@berkeley.edu>
2017-08-23 12:51:45 -0400

    added documents data

--- code/process_raw_data.R
+++ code/process_raw_data.R
@@ -1,4 +1,13 @@
 library(tidyverse)
 source("./code/functions/process_helpers.R")
 
-documents <- extract_full_text_id("./data/raw/ProQuestDocuments-2017-08-21-1-1000.txt")
+text_files <- list.files(path = "./data/raw",pattern = ".*.txt")
+
+all_documents <- c()
+for(i in 1:length(text_files)){
+  documents <- extract_full_text_id(paste0("./data/raw/", text_files[i]))
+  all_documents <- c(all_documents, documents)
+}
+
+save(all_documents, file = "./data/all_documents.Rda")
+
