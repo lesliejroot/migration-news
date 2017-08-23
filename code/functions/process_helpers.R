1b9844270d6cc0695e4440f0fd4045a262b4b094
mjalexander <monicaalexander@berkeley.edu>
2017-08-23 12:13:16 -0400

    wrote a function that extracts full text from txt files

---
+++ code/functions/process_helpers.R
@@ -0,0 +1,36 @@
+## functions to help with processing raw documents data
+
+extract_full_text_id <- function(document_text_file # the name of a text file containing documents pulled from ProQuest
+                                 ){
+  
+  tx <- read_lines(document_text_file)
+  start_of_text <- grep("Full text: ", tx)
+  sep_lines <- grep("___________________", tx) # every document is separated by these underscores
+  
+  documents <- list()
+  for (i in 1:length(start_of_text)){
+    bookends <- tx[sep_lines[i]:sep_lines[i+1]]
+    has_ID <- length(grep("ProQuest document ID:", bookends))>0
+    
+    # get full text
+    initial_pull <- tx[start_of_text[i]:(sep_lines[i+1]-1)]
+    start_of_meta <- match("", initial_pull)
+    # note: assuming the start of the meta data starts after the first blank line ("") is imperfect 
+    # alot of the documents have credits, photo info etc at the end
+    # it will do for now. 
+    final_pull <- initial_pull[1:(start_of_meta-1)]
+    
+    if(has_ID){
+      # get unique ID
+      storeID_text <- bookends[grep("ProQuest document ID:", bookends)]
+      storeID <- as.numeric(gsub(pattern = "ProQuest document ID: ","", storeID_text))
+    }
+    if(!has_ID){
+      storeID_text <- bookends[grep("https://search.proquest.com/docview/", bookends)][1]
+      storeID <- as.numeric(gsub(pattern = "https://search.proquest.com/docview/|(\\?.*)","", storeID_text))
+    }
+    documents[[i]] <- list(text = final_pull, storeID = storeID)
+  }
+  
+  return(documents)
+}
---
+++ code/process_raw_data.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+source("./code/functions/process_helpers.R")
+
+documents <- extract_full_text_id("./data/raw/ProQuestDocuments-2017-08-21-1-1000.txt")
