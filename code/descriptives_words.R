557f033417bdce06412dd689735fa62bd25601bf
mjalexander <monicaalexander@berkeley.edu>
2017-08-31 08:57:46 -0400

    plot of document category

--- code/descriptives_words.R
+++ code/descriptives_words.R
@@ -29,4 +29,21 @@ for(i in 1:length(csv_files)){
 
 all_meta <- left_join(meta_df, meta_word_df, by = c("StoreId" = "id"))
 
-# categorize 
+# categorize article as either population or immigration-related based on the counts
+
+all_meta <- all_meta %>% 
+  mutate(document_category = case_when(
+    immigra_count > population_count ~ "immigration",
+    population_count > immigra_count ~ "population",
+    TRUE ~ "both"
+  ))
+
+ggplot(all_meta %>% 
+         group_by(year, document_category) %>% 
+         summarise(n = n()), aes(year, n, color = document_category)) + 
+  geom_line() + geom_point() + 
+  scale_color_brewer(palette = "Set2") + 
+  theme_bw()+
+  ylab("number of articles")+
+  ggtitle("Number of articles by category (based on more common keyword)")
+ggsave("plots/category_year.pdf")
