7f4ef309b3bc98089b7abfc50c21284b5fe9cf5f
MJAlexander <monicaalexander@berkeley.edu>
2017-09-10 18:29:41 +1000

    small changes

--- code/sentiment.R
+++ code/sentiment.R
@@ -3,6 +3,7 @@ library(tidytext)
 
 
 # 1. Load in all the word tidy dfs ----------------------------------------
+## NOTE TO SELF: make this a function?
 
 
 word_df <- c()
@@ -11,10 +12,10 @@ for(i in 1:5){
   load(paste0("./data/word_counts_", i, ".Rda"))
   word_df <- rbind(word_df, all_word_counts_df)
 }
+rm(all_word_counts_df)
 
 ## also load in meta data to get document IDs
 
-## load in all meta data from word counts
 ## load in all meta data from word counts
 
 meta_word_df <- c()
@@ -23,7 +24,7 @@ for(i in 1:5){
   load(paste0("./data/meta_data_", i, ".Rda"))
   meta_word_df <- rbind(meta_word_df, all_meta_df)
 }
-
+rm(all_meta_df)
 
 ## load in existing meta data that was pulled from ProQuest
 
@@ -36,11 +37,13 @@ for(i in 1:length(csv_files)){
   this_meta <- read_csv(csv_files[i])
   meta_df <- bind_rows(meta_df, this_meta )
 }
-
+rm(this_meta)
 
 ## join the two meta dfs
 
 all_meta <- left_join(meta_df, meta_word_df, by = c("StoreId" = "id"))
+
+rm(meta_df, meta_word_df)
 
 # categorize article as either population or immigration-related based on the counts
 
@@ -136,7 +139,7 @@ word_sent_year <- word_df %>%
   
 main_sent_words <- word_sent_year %>%
   group_by(year, sentiment) %>%
-  top_n(n = 5, wt = n_word) %>%
+  top_n(n = 3, wt = n_word) %>%
   ungroup() %>%
   select(word)
 
@@ -178,5 +181,9 @@ bing_and_nrc <- bind_rows(word_df %>%
 bind_rows(afinn, 
           bing_and_nrc) %>%
   ggplot(aes(index, sentiment, color = method)) + geom_line()
+
+
+
+
 
 
