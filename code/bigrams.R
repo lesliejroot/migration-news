1ab28902db5f754a1ed848baf3bbe66c80624c06
MJAlexander <monicaalexander@berkeley.edu>
2017-09-12 11:23:28 +1000

    network graph

--- code/bigrams.R
+++ code/bigrams.R
@@ -248,3 +248,35 @@ immigrant_words %>%
   ylab("Sentiment score * number of occurrences") +
   coord_flip() + theme_bw()
 ggsave("plots/bigram_immigrant_barplot.pdf", width = 10, height = 7)
+
+
+
+# 7. graphing -------------------------------------------------------------
+
+library(igraph)
+library(ggraph)
+
+bigram_graph <- bigram_counts %>%
+  filter(n > 220) %>%
+  graph_from_data_frame()
+
+bigram_graph
+set.seed(2017)
+
+ggraph(bigram_graph, layout = "fr") +
+  geom_edge_link() +
+  geom_node_point() +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1)+
+  theme_bw()
+
+set.seed(2016)
+
+a <- grid::arrow(type = "closed", length = unit(.15, "inches"))
+
+ggraph(bigram_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = FALSE,
+                 arrow = a, end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "lightblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+  theme_void()
+ggsave("./plots/bigram_network.pdf", width = 15, height = 15)
