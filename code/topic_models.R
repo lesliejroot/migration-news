2f937a498eff1f5e69b128b0eed2bfd0554b88d1
MJAlexander <monicaalexander@berkeley.edu>
2017-09-13 10:53:42 +1000

    more on topics

--- code/topic_models.R
+++ code/topic_models.R
@@ -5,8 +5,6 @@ library(topicmodels)
 
 # 1. Load in all the word tidy dfs ----------------------------------------
 ## NOTE TO SELF: make this a function?
-
-
 word_df <- c()
 
 for(i in 1:5){
@@ -61,15 +59,17 @@ all_meta <- all_meta %>%
   ))
 
 
-
-
 # join to word df 
+# NOTE: filtering out the art exhibitions. 
 word_df <- word_df %>%
-  inner_join(all_meta %>% select(document_number, StoreId))
+  inner_join(all_meta %>% select(document_number, StoreId, Title)) %>%
+  filter(Title!= "The Listings: Art", Title!="Spare Times", Title!="Spare Time") %>%
+  select(-Title)
 
 
 # 2. Cast word df as a dtm ------------------------------------------------
 
+# filter some obvious problems 
 word_dtm <- word_df %>%
   filter(word!="text", 
          word!="photo", word!="caption", word!="photograph", 
@@ -127,7 +127,7 @@ ggsave("./plots/tm_diff_2topic.pdf", width = 10, height = 7)
 
 ## more topics
 
-im_lda <- LDA(word_dtm, k = 4, control = list(seed = 1234))
+im_lda <- LDA(word_dtm, k = 10, control = list(seed = 1234))
 
 # betas are per-topic-per-word probabilities
 im_topics <- tidy(im_lda, matrix = "beta")
@@ -148,5 +148,26 @@ im_top_terms %>%
   facet_wrap(~ topic, scales = "free") +
   coord_flip()+
   theme_bw()+
-  ggtitle("Top words in a four topic model")
-ggsave("./plots/tm_4topic.pdf", width = 10, height = 7)
+  ggtitle("Top words in a ten topic model")
+ggsave("./plots/tm_10topic.pdf", width = 10, height = 7)
+
+
+## find articles that match these topics?
+
+
+im_gamma <- tidy(im_lda, matrix = "gamma")
+
+
+example_docs <- im_gamma %>% 
+  group_by(topic) %>%
+  top_n(30, gamma) %>%
+  mutate(document_number = as.numeric(document)) %>%
+  inner_join(all_meta %>% select(document_number, year, StoreId, Title, Abstract, subjectTerms)) %>%
+  arrange(topic, -gamma)
+
+write_csv(example_docs, "./tables/example_documents_10topic.csv")
+
+
+
+
+
